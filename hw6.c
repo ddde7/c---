9585a4ba7a0de524fc4d98c9f06dd22863852f17
ddde7 <202010895leeseungmin@gmail.com>
2022-11-03 16:30:03 +0900

    c프로그래밍 9주차 과제제출

---
+++ hw6.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int arr[5];
+	printf("Please input five integers: ");
+	for (int i = 0; i < 5; i++)
+		scanf("%d", &arr[i]);
+	printf("Odd numbers: ");
+	for (int i = 0; i < 5; i++)
+	{
+		if (arr[i] % 2 == 1)
+			printf("%d ", arr[i]);
+	}
+	printf("\n");
+	printf("Even numbers: ");
+	for (int i = 0; i < 5; i++)
+	{
+		if (arr[i] % 2 != 1)
+			printf("%d ", arr[i]);
+	}
+	
+
+}
